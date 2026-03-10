a41f5427cbde69b5d693810930818647c2eaddf3
sergiotsuruda-byte <sergiotsuruda@gmail.com>
2026-03-10 08:51:21 -0300

    Update importador.js

--- importador.js
+++ importador.js
@@ -1,1 +1,16 @@
+async function carregarImoveis() {
+  const url = "https://www.joanny.com.br/banco_de_dados.xml";
 
+  try {
+    const resposta = await fetch(url);
+    const texto = await resposta.text();
+
+    console.log("XML carregado com sucesso");
+    console.log(texto.substring(0, 1000));
+
+  } catch (erro) {
+    console.error("Erro ao carregar XML:", erro);
+  }
+}
+
+carregarImoveis();
